c9859c9f9878cda1b6015c31404a3d0dfa5dd6c2
Vignesh M <vickymadeshwaran@gmail.com>
2024-02-19 17:43:26 +0530

    square_function_demo.cpp

--- Square_Function_Demo/square_function_demo.cpp
+++ Square_Function_Demo/square_function_demo.cpp
@@ -1,1 +1,21 @@
+// Square a double precision floating-point number
 
+#include<iostream>
+
+using namespace std;
+
+double square(double x)
+{
+        return x*x;
+}
+
+// The 'return type' void indicates that a function does not return a value
+void print_square(double x)
+{
+        cout<<"The Square of "<<x<<" is "<<square(x)<<"\n";
+}
+
+int main()
+{
+        print_square(1.234);
+}
